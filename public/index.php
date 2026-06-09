cbfcce5ce530be5faa3a1c76e6308f9b9ad6131a
Everesh <everesh@proton.me>
2026-06-09 23:43:29 +0200

    poc: post controller

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Dotenv\Dotenv;
 use Everesh\ZeroX45\Controller\HomeController;
+use Everesh\ZeroX45\Controller\PostController;
 use Everesh\ZeroX45\Middleware\SessionMiddleware;
 use Everesh\ZeroX45\Model\Database;
 
@@ -28,5 +29,6 @@ $dev = ($_ENV["APP_ENV"] ?? "prod") === "dev";
 $app->addErrorMiddleware($dev, true, $dev);
 
 $app->get("/", [new HomeController($view, $db), "index"]);
+$app->get("/post/{id}", [new PostController($view, $db), "show"]);
 
 $app->run();
---
+++ src/Controller/PostController.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Everesh\ZeroX45\Controller;
+
+use Doctrine\DBAL\Connection;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\PhpRenderer;
+
+class PostController
+{
+    public function __construct(
+        private readonly PhpRenderer $view,
+        private readonly Connection $db,
+    ) {}
+
+    public function show(
+        Request $request,
+        Response $response,
+        array $args,
+    ): Response {
+        // TODO
+        //
+        // for now this is just Home View trunkated to the specific post
+
+        $data = $this->db
+            ->createQueryBuilder()
+            ->select("p.*", "COALESCE(SUM(e.vote), 0) AS rating")
+            ->from("thread", "t")
+            ->leftJoin("t", "post", "p", "t.anchor_id = p.id")
+            ->leftJoin("p", "endorse", "e", "e.id_post = p.id")
+            ->groupBy("p.id")
+            ->where("p.id = ?")
+            ->setParameter(0, $args["id"])
+            ->fetchAllAssociative();
+
+        return $this->view->render($response, "home.php", [
+            "sessionId" => session_id(),
+            "data" => $data,
+        ]);
+    }
+}
