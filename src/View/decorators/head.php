8e716a50d08ce488142b9074aeba93fb903862df
Everesh <everesh@proton.me>
2026-06-11 13:56:55 +0200

    fix: mobile view scale

--- src/View/decorators/head.php
+++ src/View/decorators/head.php
@@ -9,6 +9,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= $basePath ?>/css/style.css">
     <title>0x45: <?= htmlspecialchars($title) ?></title>
     <script src="<?= $basePath ?>/js/animations.js" defer></script>
